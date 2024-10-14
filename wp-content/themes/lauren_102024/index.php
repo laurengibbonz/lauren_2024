0d8cfd111843cbe315fc998f24ac6917bb26151a
Lauren <laurenfrank@Our-MacBook-Pro.local>
2024-10-14 12:44:40 -0400

    video fix

--- wp-content/themes/lauren_102024/index.php
+++ wp-content/themes/lauren_102024/index.php
@@ -15,11 +15,11 @@ $Android = stripos($_SERVER['HTTP_USER_AGENT'],"Android"); ?>
 <header id="home__header" class="scene scene--fadein">
 <!-- <h1><?php bloginfo('name'); ?></h1> -->
 <!-- <h3><?php bloginfo('description'); ?></h3> -->
-<h3 class="headline">Lauren Gibbons Frank is a designer, teacher, and coder sculpting data-driven experiences that inspire.</h3>
+<h3 class="headline"><strong>Lauren Gibbons Frank</strong> is a designer, teacher, and coder sculpting<br/> data-driven experiences that inspire.</h3>
 </header>
 
 <div id="video">
-<video autoplay loop>
+<video autoplay loop muted>
   <source src="https://laurengibbons.com/wp-content/uploads/2024/08/GibbonsFrank.mp4" type="video/mp4">
   <source src="https://laurengibbons.com/wp-content/uploads/2024/08/GibbonsFrank.mp4" type="video/ogg">
   Your browser does not support the video tag.
